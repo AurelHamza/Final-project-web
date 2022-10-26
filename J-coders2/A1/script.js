678c77ed51ab8aa85578b6ef79cbed222e8f5024
AurelHamza <Aure.lHamza11@gmail.com>
2022-10-26 20:44:34 +0200

    Final-project-web

---
+++ J-coders2/A1/script.js
@@ -0,0 +1,22 @@
+// let x = "12"
+// let y = true
+// console.log(-y)
+// console.log(typeof x)
+// console.log(-x)
+
+let moshaNoarit;
+moshaNoarit = 2022 - 2008;
+console.log(moshaNoarit)
+let nr1 = 4, nr2 = 2;
+let mbledhja = nr1 + nr2;
+let zbrita = nr1 - nr2;
+let shumezimi = nr1 * nr2;
+let pjestimi = nr1/nr2;
+console.log("Mbledhja " + mbledhja)
+console.log("Zbritja " + zbrita)
+console.log("Shumezimi " + shumezimi)
+console.log("Pjestimi" + pjestimi)
+let fuqizimi = nr1 ** nr2;
+console.log(fuqizimi)
+let modulo = 5 % 2;
+console.log("Mbetja e 5 /2 eshte " + modulo)
